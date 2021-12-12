6a6c2a61d45b8fff0d51dfc0124d4bcf908f386d
Alioune MANE <aliounemane2@gmail.com>
2021-12-12 17:12:00 +0000

    Create ConcreteCreator.java

---
+++ ConcreteCreator.java
@@ -0,0 +1,20 @@
+package alioune.mane.factorymethod;
+
+/**
+ * 
+ * ConcreteCreator class returns an instance of the ConcreteProduct
+ *
+ */
+public class ConcreteCreator extends Creator {
+
+  public Product factoryMethod(String type) {
+
+    if (type.equals("A")) {
+      return new ConcreteProductA();
+    } else if (type.equals("B")) {
+      return new ConcreteProductB();
+    }
+
+    return null;
+  }
+}
