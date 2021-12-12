99f6d7e0148b305aab9ad1037c75e98c10600f9b
Alioune MANE <aliounemane2@gmail.com>
2021-12-12 17:14:58 +0000

    Update ConcreteCreator.java

--- ConcreteCreator.java
+++ ConcreteCreator.java
@@ -9,11 +9,10 @@ public class ConcreteCreator extends Creator {
 
   public Product factoryMethod(String type) {
 
-    if (type.equals("A")) {
+    if (type.equals("A"))
       return new ConcreteProductA();
-    } else if (type.equals("B")) {
+    else if (type.equals("B"))
       return new ConcreteProductB();
-    }
 
     return null;
   }
