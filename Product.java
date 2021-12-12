a49cbad3737b9ea8cfa5098d8510e87138dd9041
Alioune MANE <aliounemane2@gmail.com>
2021-12-12 17:16:56 +0000

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,11 @@
+package alioune.mane.factorymethod;
+
+/**
+ * 
+ * Product interface, defines the interface of the objects which factory method
+ * creates.
+ *
+ */
+public interface Product {
+
+}
